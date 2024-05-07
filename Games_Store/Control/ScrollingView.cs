0b15b801a408ecc455f1e75cecc116782240e4a1
Jake Smith <S00234729@atu.ie>
2024-05-07 21:05:12 +0100

    Final Commit

---
+++ Games_Store/Control/ScrollingView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Games_Store.Control
+{
+    public class ScrollingView : ScrollViewer
+    {
+
+
+
+        public double MyOffset
+        {
+            get { return (double)GetValue(ScrollViewer.HorizontalOffsetProperty); }
+            set { this.ScrollToHorizontalOffset(value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Offset.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyOffsetProperty =
+            DependencyProperty.Register("MyOffset", typeof(double), typeof(ScrollingView), new PropertyMetadata(new PropertyChangedCallback(OnChange)));
+
+        private static void OnChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((ScrollingView)d).MyOffset = (double)e.NewValue;
+        }
+    }
+}
--- Games_Store/MainWindow.xaml.cs
+++ Games_Store/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Games_Store.Control;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -34,10 +36,7 @@ namespace Games_Store
             }
         }
 
-        private void MainWindow_MediaEnded(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
@@ -71,6 +70,54 @@ namespace Games_Store
         public static readonly DependencyProperty IsMenuExpandedProperty =
             DependencyProperty.Register("IsMenuExpanded", typeof(bool), typeof(MainWindow), new PropertyMetadata(true));
 
+        private void btn_NavLeft_Click(object sender, RoutedEventArgs e)
+        {
+            btn_NavLeft.IsEnabled = false;
+            btn_NavRight.Visibility = Visibility.Visible;
+
+            double offset = (double)scrollViewer.GetValue(ScrollingView.MyOffsetProperty);
+            DoubleAnimation leftmove = new DoubleAnimation(offset, offset - 246 - 20, new Duration(TimeSpan.FromSeconds(.5))) { AccelerationRatio = .2, DecelerationRatio = .8 };
+
+            leftmove.Completed += Left_Complete;
+            scrollViewer.BeginAnimation(ScrollingView.MyOffsetProperty, leftmove);
+
+        }
+
+        private void btn_NavRight_Click(object sender, RoutedEventArgs e)
+        {
+            btn_NavRight.IsEnabled = false;
+            IsMenuExpanded = false;
+            btn_NavLeft.Visibility = Visibility.Visible;
+
+            double offset = (double)scrollViewer.GetValue(ScrollingView.MyOffsetProperty);
+            DoubleAnimation rightmove = new DoubleAnimation(offset, offset + 246 + 20, new Duration(TimeSpan.FromSeconds(.5))) { AccelerationRatio = .2, DecelerationRatio = .8 };
+
+            rightmove.Completed += Right_Complete;
+            scrollViewer.BeginAnimation(ScrollingView.MyOffsetProperty, rightmove);
+
+        }
+
+        private void Right_Complete(object sender, EventArgs e) 
+        {
+            btn_NavRight.IsEnabled = true;
+            if (Convert.ToInt32(scrollViewer.HorizontalOffset) == Convert.ToInt32(scrollViewer.ScrollableWidth)) 
+            {
+                btn_NavRight.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Left_Complete(object sender, EventArgs e)
+        {
+            btn_NavLeft.IsEnabled = true;
+            if (Convert.ToInt32(scrollViewer.HorizontalOffset) == 0)
+            {
+                btn_NavLeft.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+
+       
 
     }
 }
